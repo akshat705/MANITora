a388fc82798e77e02e03b97915598e5101a39b60
akshat705 <59477792+akshat705@users.noreply.github.com>
2021-03-11 16:09:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+const express=require('express');
+const bodyParser=require('body-parser');
+const ejs=require('ejs');
+
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  res.render("home"); 
+});
+
+app.listen(3000,function(req,res){
+  console.log("Server started at port 3000");
+});
