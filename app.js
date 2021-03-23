44d0839f974a0d763e1741fb23e31c27c37340f1
akshat705 <59477792+akshat705@users.noreply.github.com>
2021-03-23 23:37:21 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const passportLocalMongoose=require("passport-local-mongoose"); // PP
 const app=express();
 
 var currUser;
-var questionsAndAnswers=[];
-var userArray=[];
+var isauth=0;
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 mongoose.set('useCreateIndex', true);
 
 app.use(session({                                               // PP
@@ -44,7 +44,12 @@ const questAndAnswerSchema=new mongoose.Schema({
   email:String,
   quest:String,
   postedBy: userSchema,
-  answer:[String]
+  answer:[{
+    answer:String,
+    name:String
+  }],
+  likes:Number,
+  views:Number
 });
 
 
@@ -61,25 +66,26 @@ passport.deserializeUser(function(user, done) {
 });
 
 app.get("/",function(req,res){
-  res.render("home");
+  res.render("home",{isauth:isauth,currUser:currUser});
 });
 
 app.get("/login",function(req,res){
 
-  res.render("login");
+  res.render("login",{isauth:isauth,currUser:currUser});
 });
 app.get("/signup",function(req,res){
-  res.render("signup");
+  res.render("signup",{isauth:isauth,currUser:currUser});
 });
 
 app.get("/discussion",function(req,res){
 
   if(req.isAuthenticated()){
+    isauth=1;
     Question.find({},function(err,questions){
       if(err){
         console.log(err);
       } else {
-        res.render("discussion",{questions:questions});
+        res.render("discussion",{questions:questions,isauth:isauth,currUser:currUser});
       }
     });
   }else{
@@ -88,12 +94,39 @@ app.get("/discussion",function(req,res){
 });
 app.get("/ask",function(req,res){
   if(req.isAuthenticated()){
-      res.render("ask");
+      res.render("ask",{isauth:isauth,currUser:currUser});
   }else{
     res.redirect("/login");
   }
 });
 
+app.get("/logout",function(req,res){
+  isauth=0;
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/answers/:questionId",function(req,res){
+  if(req.isAuthenticated()){
+    const questionId=req.params.questionId;
+    Question.findById(questionId,function(err,question){
+      question.views=question.views+1;
+      question.save();
+      res.render("answers",{isauth:isauth,currUser:currUser,question:question});
+    });
+  } else {
+    res.redirect("/login");
+  }
+
+});
+app.get("/likes/:questionId",function(req,res){
+  const questionId=req.params.questionId;
+  Question.findById(questionId,function(err,question){
+    question.likes=question.likes+1;
+    question.save();
+    res.redirect("/discussion");
+  });
+});
 
 app.post("/signup",function(req,res){
   User.register({
@@ -108,6 +141,7 @@ app.post("/signup",function(req,res){
       res.redirect("/signup");
     } else{
       passport.authenticate("local")(req,res,function(){
+        isauth=1;
         User.findOne({'username':req.body.username},function(err,foundUser){
           currUser=foundUser;
           //console.log(currUser);
@@ -118,7 +152,6 @@ app.post("/signup",function(req,res){
     }
   });
 });
-
 
 app.post("/login",function(req,res){
   const user= new User({
@@ -131,6 +164,7 @@ app.post("/login",function(req,res){
       res.redirect("login");
     } else{
       passport.authenticate("local")(req,res,function(){
+        isauth=1;
         User.findOne({'username':req.body.username},function(err,foundUser){
           currUser=foundUser;
           //console.log(currUser);
@@ -146,12 +180,22 @@ app.post("/ask",function(req,res){
   //console.log(req.body.quest);
   const newQuest=new Question({
     postedBy:currUser,
-    quest:req.body.quest
+    quest:req.body.quest,
+    likes:0,
+    views:0
   });
   newQuest.save();
   res.redirect("/discussion");
 });
 
+app.post("/answers/:questionId",function(req,res){
+  const questionId=req.params.questionId;
+  Question.findById(questionId,function(err,question){
+    question.answer.push({"answer":req.body.ans,"name":currUser.name});
+    question.save();
+    res.redirect("/answers/"+question._id);
+  });
+});
 
 app.listen(3000,function(req,res) {
   console.log("Server started at port 3000");
